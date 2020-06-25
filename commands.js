2cb85a7e2774917132d053c6d155b528807f34e9
Braian Dickson <braiandickson@gmail.com>
2020-06-25 16:52:01 -0300

    Refactor randomMsgs.

--- commands.js
+++ commands.js
@@ -1,13 +1,11 @@
 const _ = require('lodash');
 const { commandMessage, defaultMsg, imgs, greatings, randoms } = require('./messages');
 
-const getRandomMsg = messages => messages && _.sample(messages);
-
 const commandsDeclaration = ['comandos', 'hola', 'foto'];
 
 const commands = {
-  hola: getRandomMsg(greatings),
-  foto: getRandomMsg(imgs),
+  hola: _.sample(greatings),
+  foto: _.sample(imgs),
   randoms: randoms,
   comandos: [
     `${commandMessage} ${commandsDeclaration}`
